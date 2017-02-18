d30a532fec01e7635f1ac150f2529ff54d9899d6
Jorji Nonaka <jorji@nonaka-hp.aics27.riken.jp>
2017-02-18 16:36:28 +0900

    Travis Test 8

--- travis_setup.sh
+++ travis_setup.sh
@@ -10,7 +10,7 @@ else	# guess linux
 
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update
-	sudo apt-get install -yq build-essential gcc-4.8 g++-4.8 make
+	sudo apt-get install -yq build-essential gcc-4.8 g++-4.8 cmake make bison flex libpthread-stubs0-dev
 	sudo apt-get install -qq openmpi-bin libopenmpi-dev
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
