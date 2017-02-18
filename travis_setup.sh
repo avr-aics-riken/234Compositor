8e73efe5b04e5bbf92a3277863c3b372c4bd03b8
Jorji Nonaka <jorji@nonaka-hp.aics27.riken.jp>
2017-02-18 16:22:17 +0900

    Revert "Travis Test 7"
    This reverts commit 698a6724509078d8bceb3d62f6a5ea6aae2baa87.

--- travis_setup.sh
+++ travis_setup.sh
@@ -4,14 +4,15 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
 	brew update
 	brew install gcc48 
-	brew install openmpi
+	brew install openmpi --build-from-source
+	brew install homebrew/science/hdf5
 
 else	# guess linux
 
 	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update
 	sudo apt-get install -yq build-essential gcc-4.8 g++-4.8 cmake make bison flex libpthread-stubs0-dev
-	sudo apt-get install -qq openmpi-bin libopenmpi-dev
+	sudo apt-get install -qq "openmpi-bin libopenmpi-dev"
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8
 	echo 2 | sudo update-alternatives --config gcc
