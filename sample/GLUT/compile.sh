45df1c6ff3ab8b391afee21763c2d493ecf3e844
Jorji Nonaka <jorji@riken.jp>
2017-06-30 13:14:31 +0900

    BugFix link order

--- sample/GLUT/compile.sh
+++ sample/GLUT/compile.sh
@@ -1,3 +1,3 @@
-mpicc -std=gnu99 -Wall -fopenmp -I../../include -o test_234byte  test_234byte.c  -lm ../../lib/lib234comp.a
-mpicc -std=gnu99 -Wall -fopenmp -I../../include -o test_234float test_234float.c -lm ../../lib/lib234comp.a
+mpicc -std=gnu99 -Wall -fopenmp -I../../include -o test_234byte  test_234byte.c  ../../lib/lib234comp.a -lm
+mpicc -std=gnu99 -Wall -fopenmp -I../../include -o test_234float test_234float.c ../../lib/lib234comp.a -lm
 
